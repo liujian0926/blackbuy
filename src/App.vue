1fe581dd80bb37723141721bc9a8a9e5735fe40a
liujian <329751289@qq.com>
2019-03-28 21:02:21 +0800

    会员中心页

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,11 @@
                         <strong>|</strong>
                     </span>
                     <span>
-                        <a href="" class="">会员中心</a>
+                       <router-link to="/userContainer">
+                       会员中心
+                       </router-link>
+                        
+                        
                         <strong>|</strong>
                         <a>退出</a>
                         <strong>|</strong>
--- src/main.js
+++ src/main.js
@@ -34,13 +34,29 @@ import index from './components/index.vue';
 import car from './components/car.vue';
 // 导入商品详情组件
 import detail from './components/detail.vue';
+// 导入会员中心容器组件
+import userContainer from './components/userContainer.vue';
+// 导入会员中心信息路由
+import userIndex from './components/userIndex.vue'
+import userOder from './components/userOder.vue'
+import userDetail from './components/userDetail.vue'
 
 // 路由规则
 var routes = [
   { path: '/', component: index },
   { path:'/index',component:index},
   { path: '/car', component: car },
-  {path:'/detail/:id',component:detail}
+  {path:'/detail/:id',component:detail},
+  
+  { 
+    path:'/userContainer', 
+    component: userContainer,
+    children:[
+      { path:'userIndex',component: userIndex},
+      { path: 'userOder', component: userOder},
+      { path: 'userDetail', component: userDetail}
+    ]
+}
 ];
 // 创建路由对象
 let router = new VueRouter({
